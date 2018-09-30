c6d312a1c6b6938bb15629b7d0b9e5ecee67607e
bill-coman <bcoman@tcd.ie>
2018-09-30 16:03:58 +0100

    Added put/contains/get methods and tests... Unfinished

--- LCA-Project/src/LCA.java
+++ LCA-Project/src/LCA.java
@@ -1,6 +1,5 @@
 
-
-public class LCA {
+public class LCA<Key extends Comparable<Key>, Value> {
 	
     private Node root;             // root node of BST
 
@@ -8,17 +7,84 @@ public class LCA {
      * Private node class.
      */
     private class Node {
-        private int key;           // sorted by key        // associated data
+    	private Key key;           // sorted by key
+        private Value val;         // associated data
         private Node left, right;  // left and right subtrees
-        private int N;             // number of nodes in subtree
+        private int N;             // number of nodes in subtree            // number of nodes in subtree
 
     /**
      *Node Constructor.
      */
-        public Node(int key, int N) {
-            this.key = key;
+        public Node(Key key, Value val, int N) {
+        	this.key = key;
+            this.val = val;
             this.N = N;
         }
+    }
+    
+    public boolean contains(Key key) 
+    {
+        return get(key) != null;
+    }
+
+    /**
+     *  Search BST for given key.
+     *  What is the value associated with given key?
+     *
+     *  @param key the search key
+     *  @return value associated with the given key if found, or null if no such key exists.
+     */
+    public Value get(Key key) 
+    {
+    	return get(root, key); 
+    }
+
+    private Value get(Node x, Key key) {
+        if (x == null) 
+        	return null;
+        int cmp = key.compareTo(x.key);
+		if (cmp < 0) 
+		{
+			return get(x.left, key);
+		}
+        else if(cmp > 0)
+        {
+        	return get(x.right, key);
+        }
+        else
+        {
+        	return x.val;
+        }
+    }
+    
+    public void put(Key key, Value val) {
+        if (val == null) 
+        {
+        	return; 
+        }
+        root = put(root, key, val);
+    }
+
+    private Node put(Node x, Key key, Value val) {
+        if (x == null) 
+        {
+        	return new Node(key, val, 1);
+        }
+        int cmp = key.compareTo(x.key);
+        if(cmp < 0)
+        {
+        	x.left  = put(x.left,  key, val);
+        }
+        else if(cmp > 0)
+        {
+        	x.right = put(x.right, key, val);
+        }
+        else
+        {
+        	x.val   = val;
+        }
+        x.N = 1 + x.left.N + x.right.N;
+        return x;
     }
 
 }
--- LCA-Project/src/LCATest.java
+++ LCA-Project/src/LCATest.java
@@ -3,6 +3,40 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class LCATest {
+	 @Test
+	 public void testContains() {
+	        LCA<Integer, Integer> lca = new LCA<Integer, Integer>();
+
+	        assertEquals("Contains checks if an empty tree is false", false, lca.contains(6));
+
+	        lca.put(5, 5);
+	        lca.put(6, 6);
+	        lca.put(4, 4);
+
+	        assertEquals("Contains finds if an element exists in the tree", true, lca.contains(6));
+	        assertEquals("Contains finds if an element exists in the tree", true, lca.contains(4));
+	        assertEquals("Contains finds if an element exists in the tree", true, lca.contains(5));
+	        assertEquals("Contains finds if an element exists in the tree", false, lca.contains(11));
+	    }
+	 
+	 
+	 
+	 @Test
+	 public void testPut() {
+	        LCA<Integer, Integer> lca = new LCA<Integer, Integer>();
+
+	        lca.put(6, 6);
+	        assertEquals("Put makes an element exist", true, lca.contains(6));
+	        
+	        lca.put(7, 7);
+	        assertEquals("Put makes an element exist", true, lca.contains(7));
+	        
+	        lca.put(55, 55);
+	        assertEquals("Put makes an element exist", true, lca.contains(55));
+
+	        lca.put(4, 4);
+	        assertEquals("Put makes an element exist", true, lca.contains(4));
+	    }
 
 	
 }
