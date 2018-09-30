e0d2dcbd58cc9b9d6f67b5eb84fecfe759283206
bill-coman <bcoman@tcd.ie>
2018-09-30 15:45:01 +0100

    Created nested node class with int key being used to sort.

--- LCA-Project/src/LCA.java
+++ LCA-Project/src/LCA.java
@@ -1,4 +1,24 @@
 
+
 public class LCA {
+	
+    private Node root;             // root node of BST
+
+    /**
+     * Private node class.
+     */
+    private class Node {
+        private int key;           // sorted by key        // associated data
+        private Node left, right;  // left and right subtrees
+        private int N;             // number of nodes in subtree
+
+    /**
+     *Node Constructor.
+     */
+        public Node(int key, int N) {
+            this.key = key;
+            this.N = N;
+        }
+    }
 
 }
