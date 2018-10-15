62a660e35e18d3f0547c8c0c9955b9209a227298
Bill Coman <bcoman@tcd.ie>
2018-10-15 11:10:12 +0000

    test creation

---
+++ LCA-DAG/src/CreateGraphTest.java
@@ -0,0 +1,22 @@
+class CreateGraphTest {
+
+  @Test
+  void testCreateGraph() {
+      String filename = "C:\\Bill\\MSISS JS\\SW ENGINEERING\\DiGraph LCA\\src\\DAG.txt";
+      CreateGraph g = new CreateGraph(filename);
+      
+      int[]check = new int[10];//ten nodes in example graph
+      check=g.returnLevels();
+      assertTrue(check[0] == 0);
+      assertTrue(check[1] == 1);
+      assertTrue(check[2] == 1);
+      assertTrue(check[3] == 1);
+      assertTrue(check[4] == 2);
+      assertTrue(check[5] == 2);
+      assertTrue(check[6] == 2);
+      assertTrue(check[7] == 2);
+      assertTrue(check[8] == 3);
+      assertTrue(check[9] == 3);
+      
+  }
+}
