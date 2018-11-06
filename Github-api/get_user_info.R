8cc561e0cb27cf04d65814db59395d8c3214e0fe
Bill Coman <bcoman@tcd.ie>
2018-11-06 12:41:20 +0000

    Start github api

---
+++ Github-api/get_user_info.R
@@ -0,0 +1,43 @@
+install.packages("jsonlite")
+library(jsonlite)
+install.packages("httpuv")
+library(httpuv)
+install.packages("httr")
+library(httr)
+
+ # Can be github, linkedin etc depending on application
+oauth_endpoints("github")
+
+ # Change based on what your application is
+github_app = oauth_app(appname = "SW-APP",
+                  key = "58f19bd30ba3dc77b571",
+                  secret = "342fcd11765297dc79346bebc39992203d24778b")
+
+ # Get OAuth credentials
+github_token <- oauth2.0_token(oauth_endpoints("github"), github_app)
+
+
+
+#halt here to cache, select Yes...
+
+
+
+ # Use API
+gtoken <- config(token = github_token)
+req <- GET("https://api.github.com/users/bill-coman/repos", gtoken)
+
+ # Take action on http error
+stop_for_status(req)
+
+ # Extract content from a request
+json1 = content(req)
+
+ # Convert to a data.frame
+gitDF = jsonlite::fromJSON(jsonlite::toJSON(json1))
+
+ # Subset data.frame
+gitDF[gitDF$full_name == "bill-coman/LCA-Project", "created_at"] 
+
+ # The code above was sourced from Michael Galarnyk's blog, found at:
+# https://towardsdatascience.com/accessing-data-from-github-api-using-r-3633fb62cb08
+
